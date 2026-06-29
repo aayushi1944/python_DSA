db8b4aed8062d0154841df6429257eec51c6fe5f
aayushi1944 <patelayushi1928@gmail.com>
2026-06-29 21:37:41 +0530

    first commit

---
+++ Day_1.py
@@ -0,0 +1,13 @@
+"""
+H1) An Amazon fulfilment centre has a conveyor belt with exactly 8 slots numbered 0-7. Each slot holds one product. The warehouse manager needs to: check what's at a slot, find a product, update a slot, and check if the belt is full. The conveyor belt - fixed 8 slots
+h2) Toll Plaza Simulation (Circular Queue)
+A toll plaza has a fixed capacity of 5 vehicles. If full, new vehicles must wait Implement a Circular Queue to simulate this, since it reuses empty slots without wasting memory.
+H3) The GPS Navigation System (Backtracking)
+You're building a GPS app like Google Maps for a hiking trail. The hiker moves through checkpoints. If they take a wrong tum, they hit "Go Back" to return to the previous checkpoint. But once they go back, they can also "Go Forward" if they change their mind again just like a browser's back/forward buttons
+
+Operations:
+visit(place)-move to a new place
+back()- go to previous place
+forward-go forward if available
+day 1
+"""
