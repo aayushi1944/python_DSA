00c345b7735b3e7b607750a3bc4851dcf6442803
aayushi1944 <patelayushi1928@gmail.com>
2026-07-22 18:23:03 +0530

    LL Done

--- DSA/LinkedList.py
+++ DSA/LinkedList.py
@@ -61,16 +61,52 @@ class LinkedList:
         tmp.next = newNode
     
     def deleteFirst(self):
+        if self.head  == None:
+            print("Empty Linkedlist")
+            return
+        print(self.head.data,"Removed")
+        self.head = self.head.next
         
 
-
-
-
-
-
-
-
+    def deleteLast(self):
+        if self.head  == None:
+            print("Empty Linkedlist")
+            return
+        if self.head.next == None:
+            self.deleteFirst()
+            return
+        tmp=self.head
+        prev = tmp
+        while tmp.next:
+            prev =tmp
+            tmp=tmp.next
+        print(tmp.data,"removed")
+        prev.next=None
+        self.temp = prev
+        
     
+    def deleteAtPosition(self,pos):
+        if self.head  == None:
+            print("Empty Linkedlist")
+            return
+        
+        if pos==1:
+            self.deleteFirst()
+            return
+        
+        c=1
+        tmp = self.head
+        while  c<pos-1 and tmp.next != None:
+            tmp = tmp.next
+            c+=1
+        
+        if tmp.next == None:
+            print("position out of range")
+            return
+        print(tmp.next.data,"Element removed ")
+        tmp.next=tmp.next.next
+        
+   
     def traverse(self):
         if self.head == None:
             print("linked list is empty")
@@ -114,6 +150,13 @@ while True:
         pos=int(input("Enter Position:"))  
         data=int(input("Enter Data:"))  
         ll.insertPosition(data,pos)
+    elif ch== 6:        
+        ll.deleteFirst()
+    elif ch == 7:
+        ll.deleteLast()
+    elif  ch == 8:
+        pos=int(input("Enter Position:"))  
+        ll.deleteAtPosition(pos)
 
 
 
