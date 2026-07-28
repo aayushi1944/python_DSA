285651ab8e1ba43a7c2569c39e346f203a081d5b
aayushi1944 <patelayushi1928@gmail.com>
2026-07-28 16:52:33 +0530

    SCLL done

--- DSA/SinglyCircularLL.py
+++ DSA/SinglyCircularLL.py
@@ -13,30 +13,48 @@ class SinglyCircularLL:
         if self.head is None:
             self.head = newNode
             self.tmp = newNode
+            self.tmp.next = self.head
+            print(data,"inerted")
             return
         newNode.next = self.head
         self.tmp.next = newNode
         self.tmp = newNode
+        print(data,"inerted")
 
     def traverseLL(self):
+        if self.head is None:
+            print("Empty Linked list")
+            return
+        
         t1= self.head
-        while t1.next != self.head:
+        while True:
             print(t1.data, end="->")
             t1 = t1.next
-        print(t1.data)
+            if t1 == self.head:
+                break
+            
+        
 
     def insertFirst(self, data):
+        if self.head is None:
+            self.createLL(data)
+            return
 
         newNode = Node(data)
         newNode.next = self.head
         self.head = newNode
         self.tmp.next = self.head
+        print(data,"inerted at First")
 
     def insertLast(self, data):
+        if self.head is None:
+            self.createLL(data)
+            return
         newNode = Node(data)
         newNode.next = self.head
         self.tmp.next = newNode
         self.tmp = newNode
+        print(data,"inerted at last")
     def insertAtPosition(self,pos,data):
         newNode = Node(data)
         t1 = self.head
@@ -52,10 +70,54 @@ class SinglyCircularLL:
             return
         newNode.next = t1.next
         t1.next = newNode
+        print(data,"inerted at",pos)
 
     def deleteFromFirst(self):
+        if self.head is None:
+            print("Empty Linked list")
+            return
+        if self.head.next == self.head:
+            print(self.head.data,"Removed")
+            self.head = None
+            self.tmp = None
+            return
+        print(self.head.data,"Removed")
         self.head = self.head.next
-        self.tmp = self.head
+        self.tmp.next = self.head
+
+    def  deleteFromLast(self):
+        if self.head is None:
+            print("Empty Linked list")
+            return
+        t1 = self.head
+        while t1.next != self.tmp:
+            t1 = t1.next
+        print(self.tmp.data,"is removed")
+        t1.next = self.head
+        self.tmp = t1
+
+    def deleteFromPos(self, pos):
+        if self.head is None:
+            print("Empty Linked list")
+            return
+        if pos == 1:
+            self.deleteFromFirst()
+            return
+        c=1
+        t1= self.head
+
+        while c < pos-1 and t1.next != self.head:
+            c+=1
+            t1 = t1.next
+        if t1.next == self.head:
+            print("position outof range...!")
+            return
+        if t1.next.next == self.head:
+            self.deleteFromLast()
+            return
+        print(t1.next.data,"removed")
+        t1.next = t1.next.next
+
 
         
 
@@ -81,7 +143,11 @@ while True:
         cll.insertAtPosition(pos,data)
     elif ch==6:
         cll.deleteFromFirst()
-    
+    elif ch == 7:
+        cll.deleteFromLast()
+    elif ch == 8:
+        pos = int(input("Enter Position:"))
+        cll.deleteFromPos(pos)
     
     elif ch == 0:
         break
